d660d33ef7b392469f9d688751bf832a892db8fc
Beaglefoot <ch-e-st@yandex.ru>
2022-01-06 15:47:28 +0300

    Replace Jest with Mocha
    It turns out Jest runs poorly with tree-sitter.
    
    https://github.com/tree-sitter/node-tree-sitter/issues/53
    
    None of the mentioned workarounds worked.

--- tests_config/run_spec.js
+++ tests_config/run_spec.js
@@ -1,90 +1,120 @@
 // source: https://github.com/prettier/prettier/blob/ee2839bacbf6a52d004fa2f0373b732f6f191ccc/tests_config/run_spec.js
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
-
-const prettier = require('prettier');
+const fs = require('fs')
+const path = require('path')
+const prettier = require('prettier')
+const assert = require('assert')
 
 function run_spec(dirname, options) {
-  fs.readdirSync(dirname).forEach(filename => {
-    const filepath = dirname + '/' + filename;
-    if (
-      path.extname(filename) !== '.snap' &&
-      fs.lstatSync(filepath).isFile() &&
-      filename[0] !== '.' &&
-      filename !== 'jsfmt.spec.js'
-    ) {
-      let rangeStart = 0;
-      let rangeEnd = Infinity;
-      let cursorOffset;
-      const source = read(filepath)
-        .replace(/\r\n/g, '\n')
-        .replace('<<<PRETTIER_RANGE_START>>>', (match, offset) => {
-          rangeStart = offset;
-          return '';
+  describe(path.basename(dirname), () => {
+    fs.readdirSync(dirname)
+      .filter((filename) => {
+        const filepath = dirname + '/' + filename
+        return (
+          path.extname(filename) !== '.snap' &&
+          fs.lstatSync(filepath).isFile() &&
+          filename[0] !== '.' &&
+          filename !== 'jsfmt.spec.js'
+        )
+      })
+      .forEach((filename) => {
+        it(filename, () => {
+          const filepath = dirname + '/' + filename
+
+          let rangeStart = 0
+          let rangeEnd = Infinity
+          let cursorOffset
+
+          const source = read(filepath)
+            .replace(/\r\n/g, '\n')
+            .replace('<<<PRETTIER_RANGE_START>>>', (match, offset) => {
+              rangeStart = offset
+              return ''
+            })
+            .replace('<<<PRETTIER_RANGE_END>>>', (match, offset) => {
+              rangeEnd = offset
+              return ''
+            })
+
+          const input = source.replace('<|>', (match, offset) => {
+            cursorOffset = offset
+            return ''
+          })
+
+          const mergedOptions = Object.assign(mergeDefaultOptions(options || {}), {
+            filepath,
+            rangeStart,
+            rangeEnd,
+            cursorOffset,
+          })
+
+          const output = prettyprint(input, mergedOptions)
+          const snapshot = getSnapshot(dirname, filename)
+          const actual = assembleSnapshotFormat(source, output, mergedOptions.printWidth)
+
+          if (!snapshot) {
+            writeSnapshot(dirname, filename, actual)
+            return
+          }
+
+          assert.equal(actual, snapshot)
         })
-        .replace('<<<PRETTIER_RANGE_END>>>', (match, offset) => {
-          rangeEnd = offset;
-          return '';
-        });
-
-      const input = source.replace('<|>', (match, offset) => {
-        cursorOffset = offset;
-        return '';
-      });
-
-      const mergedOptions = Object.assign(mergeDefaultOptions(options || {}), {
-        filepath,
-        rangeStart,
-        rangeEnd,
-        cursorOffset
-      });
-      const output = prettyprint(input, mergedOptions);
-      test(filename, () => {
-        expect(
-          raw(source + '~'.repeat(mergedOptions.printWidth) + '\n' + output)
-        ).toMatchSnapshot();
-      });
-    }
-  });
+      })
+  })
 }
 
-global.run_spec = run_spec;
+global.run_spec = run_spec
 
 function prettyprint(src, options) {
-  const result = prettier.formatWithCursor(src, options);
+  const result = prettier.formatWithCursor(src, options)
+
   if (options.cursorOffset >= 0) {
     result.formatted =
       result.formatted.slice(0, result.cursorOffset) +
       '<|>' +
-      result.formatted.slice(result.cursorOffset);
+      result.formatted.slice(result.cursorOffset)
   }
-  return result.formatted;
+
+  return result.formatted
 }
 
 function read(filename) {
-  return fs.readFileSync(filename, 'utf8');
-}
-
-/**
- * Wraps a string in a marker object that is used by `./raw-serializer.js` to
- * directly print that string in a snapshot without escaping all double quotes.
- * Backticks will still be escaped.
- */
-function raw(string) {
-  if (typeof string !== 'string') {
-    throw new Error('Raw snapshots have to be strings.');
-  }
-  return { [Symbol.for('raw')]: string };
+  return fs.readFileSync(filename, 'utf8')
 }
 
 function mergeDefaultOptions(parserConfig) {
   return Object.assign(
     {
       plugins: [path.dirname(__dirname)],
-      printWidth: 80
+      printWidth: 80,
     },
-    parserConfig
-  );
+    parserConfig,
+  )
+}
+
+function getSnapshotPath(dirnameWithTest, filename) {
+  return path.join(dirnameWithTest, '__snapshots__', `${filename}.snap`)
+}
+
+function getSnapshot(dirnameWithTest, filename) {
+  const snapshotPath = getSnapshotPath(dirnameWithTest, filename)
+
+  if (!fs.existsSync(snapshotPath)) return null
+
+  return read(snapshotPath)
+}
+
+function writeSnapshot(dirnameWithTest, filename, snapshotText) {
+  const snapshotPath = getSnapshotPath(dirnameWithTest, filename)
+
+  if (!fs.existsSync('__snapshots__')) fs.mkdirSync('__snapshots__')
+
+  fs.writeFileSync(snapshotPath, snapshotText)
+
+  console.log(`${path.basename(snapshotPath)} was written`)
+}
+
+function assembleSnapshotFormat(source, output, printWidth) {
+  return source + '~'.repeat(printWidth) + '\n' + output
 }
